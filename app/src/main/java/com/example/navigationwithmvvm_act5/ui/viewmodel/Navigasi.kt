90bd12621e216d79e8833df0f3b41735a267074d
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-21 16:49:39 +0700

    menambahkan dependency untuk navHostController dan rememberNavController

--- app/src/main/java/com/example/navigationwithmvvm_act5/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/navigationwithmvvm_act5/ui/viewmodel/Navigasi.kt
@@ -1,6 +1,23 @@
 package com.example.navigationwithmvvm_act5.ui.viewmodel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.core.view.MenuHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+
+
 }

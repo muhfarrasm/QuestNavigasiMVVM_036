3448957532df300f616f1670164add009550dd5a
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-21 16:34:53 +0700

    Menambahkan objek Listgender

---
+++ app/src/main/java/com/example/navigationwithmvvm_act5/model/ListGender.kt
@@ -0,0 +1,10 @@
+package com.example.navigationwithmvvm_act5.model
+
+import com.example.navigationwithmvvm_act5.R
+
+object ListGender {
+    val listGender = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}

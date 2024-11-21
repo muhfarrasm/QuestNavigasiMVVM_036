ca7f2ef9d03849e2b49866d869f6526068d5cd16
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-21 16:58:15 +0700

    membuat scaffold

--- app/src/main/java/com/example/navigationwithmvvm_act5/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/navigationwithmvvm_act5/ui/viewmodel/Navigasi.kt
@@ -1,11 +1,16 @@
 package com.example.navigationwithmvvm_act5.ui.viewmodel
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.core.view.MenuHost
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 enum class Halaman{
@@ -19,6 +24,13 @@ fun Navigasi(
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
 ) {
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+
+
+
+    }
+
 
 
 

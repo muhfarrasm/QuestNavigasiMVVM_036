5ad53d7c14a0438c7a02c7e3649f27e423fa8620
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-21 17:06:56 +0700

    membuat composable route

--- app/src/main/java/com/example/navigationwithmvvm_act5/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/navigationwithmvvm_act5/ui/viewmodel/Navigasi.kt
@@ -7,11 +7,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.MenuHost
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigationwithmvvm_act5.model.ListGender
+import com.example.navigationwithmvvm_act5.ui.view.FormMahasiswaView
 
 enum class Halaman{
     Form,
@@ -31,6 +35,16 @@ fun Navigasi(
             navController = navHost,
             startDestination = Halaman.Form.name
         ) {
+            composable(route = Halaman.Form.name){
+                val konteks = LocalContext.current
+                FormMahasiswaView(
+                    listgender = ListGender.listGender.map {
+                        id -> konteks.getString(id)
+                    },
+                ) {
+
+                }
+            }
 
         }
 
